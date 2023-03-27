5c97b4a2f0d4d75511da550541513c71bc75215c
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-03-27 15:41:37 +0530

    Create Path to Integrate Ledger

--- mobile-app/server/App.ts
+++ mobile-app/server/App.ts
@@ -6,6 +6,12 @@ import { authRoutes } from "./routes/Auth";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import sanitizedConfig from "./config";
+import network from "./fabric/network";
+import { Gateway, Wallets } from "fabric-network";
+import fs from "fs";
+import path from "path";
+
+console.log(network);
 
 // Init express
 const app = express();
@@ -30,6 +36,91 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/createProduct", async (req: any, res, next) => {
+  console.log("GET Request to Hello world inside app.ts");
+  console.log(req.body);
+
+  const productName = req.body.productName;
+  const id = req.body.id;
+  const price = req.body.price;
+  const quantity = req.body.quantity;
+  const location = req.body.location;
+
+  try {
+    console.log("inside network.ts");
+    // load the network configuration
+    const ccpPath = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "network",
+      "test-network",
+      "organizations",
+      "peerOrganizations",
+      "org1.example.com",
+      "connection-org1.json"
+    );
+    let ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(`${process.cwd()}/fabric`, "wallet");
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const identity = await wallet.get("appUser");
+    if (!identity) {
+      console.log(
+        'An identity for the user "appUser" does not exist in the wallet'
+      );
+      console.log("Run the registerUser.js application before retrying");
+      return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: "appUser",
+      discovery: { enabled: true, asLocalhost: true },
+    });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork("mychannel");
+
+    // Get the contract from the network.
+    const contract = network.getContract("basic");
+
+    // Submit the specified transaction.
+    // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
+    // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
+    // await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+    // console.log('Transaction has been submitted');
+
+    // // Disconnect from the gateway.
+    // await gateway.disconnect();
+    req.contract = contract;
+    // next();
+  } catch (error) {
+    console.error(`Failed to submit your requested transaction: ${error}`);
+    process.exit(1);
+  }
+  const contract = req.contract;
+  console.log(JSON.stringify({ productName, id, quantity, price, location }));
+  const result = await contract.submitTransaction(
+    "createProduct",
+    productName,
+    id,
+    quantity,
+    price,
+    location
+  );
+  console.log("result is", result.toString());
+  // const result = await contract.evaluateTransaction("ReadAsset", "asset5");
+  // const response = JSON.parse(result.toString());
+  res.json({ myResult: "Hello" });
+});
+
 // routes middlewares
 
 app.use("/api/auth", authRoutes);
@@ -38,7 +129,7 @@ app.use("/api/auth", authRoutes);
 // page not found error handling  middleware
 
 app.use("*", (req, res, next) => {
-  console.log('inside * method');
+  console.log("inside * method");
   const error = {
     status: 404,
     message: API_ENDPOINT_NOT_FOUND,
@@ -77,7 +168,7 @@ if (NODE_ENV === "development") {
 }
 
 // index to boot
-const index = async () => {
+const App = async () => {
   try {
     await mongoose.connect(MONGODB_URI as string);
     console.log("Connection to database established successfully.");
@@ -88,4 +179,4 @@ const index = async () => {
   }
 };
 
-index();
+App();
