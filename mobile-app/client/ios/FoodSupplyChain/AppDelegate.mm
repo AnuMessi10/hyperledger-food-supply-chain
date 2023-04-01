90f0471f4131d0414c9ecc50faaf5d43b09176a4
Nihar Vira <niharvira54321@gmail.com>
2023-04-01 17:42:32 +0530

    Integrate Google Maps API

--- mobile-app/client/ios/FoodSupplyChain/AppDelegate.mm
+++ mobile-app/client/ios/FoodSupplyChain/AppDelegate.mm
@@ -1,11 +1,12 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-
+#import <GoogleMaps/GoogleMaps.h> 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"GOOGLE_MAPS_API_KEY"]; // add this line using the api key obtained from Google Console
   self.moduleName = @"FoodSupplyChain";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
