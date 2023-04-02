c6f4bd33785ceca14408cffb63c398ff04d00367
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-04-02 16:18:04 +0530

    Integrate Backend with Chaincode ➕Add Routes

--- mobile-app/server/controllers/Product/index.ts
+++ mobile-app/server/controllers/Product/index.ts
@@ -9,8 +9,8 @@ const createProduct = async (req: any, res: Response, next: NextFunction) => {
 
   const { name, id, price, quantity, location, contract } = req.body;
 
-  const result = await contract.submitTransaction(
-    "createProduct",
+  const resultBytes = await contract.submitTransaction(
+    "CreateProduct",
     name,
     id,
     quantity,
@@ -18,12 +18,142 @@ const createProduct = async (req: any, res: Response, next: NextFunction) => {
     location
   );
 
-  const response = result.toString();
-  res.status(201).json(result.toString());
+  const result = resultBytes.toString();
+
+  res.status(201).json({ message: `Product with ${id} updated successfully`, product: result });
+};
+
+// --------------------- Get All Products ---------------------------------
+
+const getAllProducts = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Missing request body" });
+    }
+
+    const { contract } = req.body;
+
+    const resultBytes = await contract.evaluateTransaction(
+      "GetAllProducts"
+    );
+
+    const result = resultBytes.toString();
+
+    res.status(201).json(result);
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+// --------------------- Get A Product ---------------------------------
+
+const getProduct = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Missing request body" });
+    }
+
+    const { contract } = req.body;
+
+    const { id } = req.params;
+
+    const resultBytes = await contract.evaluateTransaction(
+      "GetProduct",
+      id
+    );
+
+    const result = resultBytes.toString();
+
+    res.status(201).json(result);
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+// --------------------- Update A Product ---------------------------------
+
+const updateProduct = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Missing request body" });
+    }
+
+    const { contract, quantity, price, name, location } = req.body;
+
+    const { id } = req.params;
+
+    const resultBytes = await contract.evaluateTransaction(
+      "UpdateProduct",
+      id,
+      quantity,
+      price,
+      name,
+      location,
+    );
+
+    const result = resultBytes.toString();
+
+    res.status(201).json({ message: `Product with ${id} updated successfully`, updatedProduct: result });
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+// --------------------- Delete A Product ---------------------------------
+
+const deleteProduct = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Missing request body" });
+    }
+
+    const { contract, id } = req.body;
+
+    await contract.evaluateTransaction(
+      "DeleteProduct",
+      id,
+    );
+
+    res.status(201).json({ message: `Product with ${id} deleted successfully` });
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+const transferProduct = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Missing request body" });
+    }
+
+    const { contract, id, newActor } = req.body;
+
+    const resultBytes = await contract.evaluateTransaction(
+      "TransferProduct",
+      id,
+      newActor
+    );
+
+    const result = resultBytes.toString();
+
+    res.status(201).json({ message: `Product with ${id} updated successfully`, updatedProduct: result });
+
+  } catch (error) {
+    throw error;
+  }
 };
 
 const productController = {
   createProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+  transferProduct,
+  deleteProduct
 };
 
 export default productController;
--- mobile-app/server/routes/Product/index.ts
+++ mobile-app/server/routes/Product/index.ts
@@ -2,10 +2,20 @@ import express from "express";
 import productController from "../../controllers/Product";
 import network from "../../fabric/network";
 
-const { createProduct } = productController;
+const { createProduct, deleteProduct, getProduct, getAllProducts, updateProduct, transferProduct } = productController;
 
 const {Router} = express;
 
 export const productRoutes = Router();
 
 productRoutes.post("/create", network.connectToNetwork, createProduct);
+
+productRoutes.get("/:id", network.connectToNetwork, getProduct);
+
+productRoutes.get("s/all", network.connectToNetwork, getAllProducts);
+
+productRoutes.patch("/update/:id", network.connectToNetwork, updateProduct);
+
+productRoutes.delete("/delete/:id", network.connectToNetwork, deleteProduct);
+
+productRoutes.post("/transfer/:id", network.connectToNetwork, transferProduct);
