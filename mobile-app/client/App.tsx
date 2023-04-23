e94f0a62faa4fddac4447d112d843f126792023e
Anurag Yadav <anuragdyadav2001@gmail.com>
2023-04-23 17:25:53 +0530

    Set Base URL on App Init

--- mobile-app/client/App.tsx
+++ mobile-app/client/App.tsx
@@ -5,13 +5,19 @@
  * @format
  */
 
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {NativeBaseProvider} from 'native-base';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStack from './src/Navigation/AuthStack';
+import AxiosUtils from './src/Utilities/axiosUtils';
+import Config from 'react-native-config';
 
 const App: FC = () => {
+  useEffect(() => {
+    AxiosUtils.setBaseAPI_URL(Config.BASE_URL ?? '');
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NativeBaseProvider>
